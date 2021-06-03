4a6373dd7faaad8a6e15c3077125f7d2a3996890
Dragan <dragan1176@gmail.com>
2021-06-03 14:27:59 +0200

    Update generate-env.sh

--- build-commands/generate-env.sh
+++ build-commands/generate-env.sh
@@ -2,4 +2,4 @@
 
 vars=$(env | grep "^[A-Za-z]" | sed "s/\([A-Za-z_]\+\)=.*/\1/" | sort)
 subst=$(printf '${%s} ' $vars)
-envsubst "$subst" < ./build/.env.${1}.template > .env
+envsubst "$subst" < ./build-commands/.env.${1}.template > .env
